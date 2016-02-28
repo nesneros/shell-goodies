2ea3c3b37a918211df87c393551d67c62e7bd2fe
Jan Sorensen <sorensenjan@gmail.com>
2016-02-28 18:44:58 +0100

    Explicit set hippie-expand-try-functions-list

--- lib/emacsinit.el
+++ lib/emacsinit.el
@@ -71,6 +71,17 @@
 
 ;;; Hippie expand
 (require 'hippie-exp)
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 (setq load-path (cons "/etc/unuxus/lib/emacs/" load-path))
 
