f358326722a809f5a718db4d00d01ec1bfcedc0f
Jan Sorensen <sorensenjan@gmail.com>
2016-03-01 22:23:58 +0100

    Option for --kill and --restart

--- bin/e.sh
+++ bin/e.sh
@@ -10,6 +10,12 @@ while [[ "$1" = -* ]] ; do
             [[ -z "$root" ]] && echo "No git repo in $d" && exit 5
             cmd=(--eval "(magit-status \"$root\")")
             ;;
+        (--kill)
+            cmd=(--eval "(kill-emacs)")
+            ;;
+        (-x|--restart)
+            emacsclient --alternate-editor '' --eval "(kill-emacs)"
+            ;;
         (--)
             shift
             break
@@ -23,5 +29,5 @@ done
 
 # See http://www.gnu.org/software/emacs/manual/html_mono/elisp.html#Frames for meaning of 'ns 
 x=$(emacsclient --alternate-editor '' --eval "(member 'ns (mapcar 'framep (frame-list)))" 2>/dev/null)
-([[ -z "$x" ]] || [[ "$x" = 'nil' ]]) && extras=--create-frame
-emacsclient $extras --no-wait "${cmd[@]}" >/dev/null
+([[ -z "$x" ]] || [[ "$x" = 'nil' ]]) && cmd=(--create-frame ${cmd[@]})
+emacsclient --no-wait "${cmd[@]}" >/dev/null
