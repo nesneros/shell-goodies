6139e5a4251841fa86aaea49f1e009b3f782a419
Jan Sorensen <sorensenjan@gmail.com>
2016-03-08 12:12:48 +0100

    Fix a few bugs in e.sh

--- bin/e.sh
+++ bin/e.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
+declare -a cmd
+
 while [[ "$1" = -* ]] ; do
     case "$1" in
         (--git)
@@ -33,16 +35,16 @@ while [[ "$1" = -* ]] ; do
     shift
 done
 
-if [[ "$git" ]]; then
+if [[ "$git" = t ]]; then
     d=$(realpath "${2:-.}")
-    shift
+    shift || true
     cd "$d"
     root="$(git rev-parse --show-toplevel 2> /dev/null)"
     [[ -z "$root" ]] && echo "No git repo in $d" && exit 5
     cmd=(--eval "(magit-status \"$root\")")
 fi
 
-cmd=(${cmd[0]} "$@")
+cmd=("${cmd[@]}" "$@")
 
 if [[ ${#@} -eq 0 ]]; then
     [[ "$noDefaultArg" = t ]] && exit 0
@@ -56,7 +58,7 @@ else
         hasFrame=$(emacsclient --eval "(member 'ns (mapcar 'framep (frame-list)))" 2>/dev/null) || :
     fi
     [[ -z "$hasFrame" || "$hasFrame" = 'nil' ]] && cmd=(--create-frame ${cmd[@]})
-    cmd=(--no-wait ${cmd[@]})
+    cmd=(--no-wait "${cmd[@]}")
 fi
 
 emacsclient --alternate-editor '' "${cmd[@]}" 2>/dev/null
