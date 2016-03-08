73c6dcd35b6a9c74d65aba70b74bb432e8a5765b
Jan Sorensen <sorensenjan@gmail.com>
2016-03-08 05:50:55 +0100

    Dired configuration

--- lib/elisp.d/dired.el
+++ lib/elisp.d/dired.el
@@ -1,23 +1,51 @@
-(setq is-darwin (string= system-type "Darwin"))
-(setq is-linux (string= system-type "gnu/linux"))
+(require 'dash)
+(defconst is-darwin (string= system-type "darwin"))
+(defconst is-linux (string= system-type "gnu/linux"))
 
 (defun os-open-file(file)
   (cond (is-darwin (call-process "open" nil 0 nil file))
         (is-linux (call-process "xdg-open" nil 0 nil file))))
 
+;; If coreutils is installed tell emacs to use gls as ls in dired mode
+(--when-let (expand-file-name "bin/gls" (or (getenv "BREW_PREFIX") "/usr/local"))
+  (when (file-exists-p it)
+    (setq insert-directory-program it)))
 
-;;; Dired
-(setq wdired-allow-to-change-permissions t)
-(setq wdired-allow-to-redirect-links t)
 (defun dired-open-file ()
   "In dired, open the file named on this line."
   (interactive)
-  (let* ((file (dired-get-filename nil t)))
+  (let ((file (dired-get-filename nil t)))
     (os-open-file file)))
-(eval-after-load "dired+" '(define-key dired-mode-map "\C-co" 'dired-open-file))
 
-;; If coreutils is installed tell emacs to use gls as ls in dired mode
-(let* ((brew-prefix (or (getenv "BREW_PREFIX") "/usr/local"))
-       (ls-prog (expand-file-name "/bin/gls" brew-prefix)))
-  (if (file-exists-p ls-prog)
-      (setq insert-directory-program ls-prog)))
+(use-package dired
+  :demand
+  :bind (:map
+         dired-mode-map
+         ("C-c o" . dired-open-file)))
+
+(use-package wdired
+  :commands wdired-mode
+  :init
+  (setq wdired-allow-to-change-permissions t)
+  (setq wdired-allow-to-redirect-links t))
+
+(use-package dired+
+  :demand t
+  :ensure t)
+
+(use-package dired-k
+  :ensure t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;; dired-hacks packages
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; (use-package dired-rainbow
+;;   :ensure t)
+
+(use-package dired-ranger
+  :ensure t)
+
+;; (use-package dired-subtree
+;;   :ensure t)
+
+
