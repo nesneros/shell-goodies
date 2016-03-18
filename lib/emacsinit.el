8bb93095cca7aa8816e1db34d3ed7a1fbd0a425e
Jan Sorensen <sorensenjan@gmail.com>
2016-03-18 17:02:21 +0100

    Define safe themes

--- lib/emacsinit.el
+++ lib/emacsinit.el
@@ -316,6 +316,13 @@
   :ensure t
   :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
+;; Mark themes as safe
+(setq custom-safe-themes
+      '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
+        "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+        "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e"
+        default))
+
 ;;; Smart mode line
 (use-package smart-mode-line
   :ensure t
