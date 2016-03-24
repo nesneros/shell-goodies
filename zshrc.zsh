7a69097e3e01341feea302a28fd416b0cc319c1e
Jan Sorensen <sorensenjan@gmail.com>
2016-03-24 17:11:05 +0100

    Delete empty lines

--- zshrc.zsh
+++ zshrc.zsh
@@ -60,10 +60,6 @@ bindkey -M emacs '^N' history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
 
-
-
-
-
 typeset -U path
 typeset -U manpath
 
