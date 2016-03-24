204dd1bd492dabf486dbf4d2e2b3a918298bb9a8
Jan Sorensen <sorensenjan@gmail.com>
2016-03-24 17:12:39 +0100

    Works with exuberant ctags, etags, and std ctags

--- lib/git_template/hooks/ctags.sh
+++ lib/git_template/hooks/ctags.sh
@@ -1,6 +1,30 @@
 #! /usr/bin/env bash
 set -e
 
+# Function to call ctags/etags
+gentags() {
+    local exuberant # Set to t if we have exuberant ctags
+    local file="$1"
+    local root="$2"
+    local cmd=etags
+    # Preference is to exuberant ctags, then ctags, and finally etags
+    if type ctags-exuberant ; then 
+        cmd=ctags-exuberant
+        exuberant=t
+    elif type ctags >/dev/null ; then
+        cmd=ctags
+        # Check is ctags is exuberant
+        ctags --version | head -n 1 | grep --silent --ignore-case excuberant && exuberant=t
+    fi
+    local a
+    if [[ "$exuberant" = t ]] ; then
+        a=(-e --tag-relative -L - -f"$file" --languages=-Vera,REXX)
+    else
+        a=(-o "$file" -)
+    fi
+    git ls-files | grep "^$root" | "$cmd" "${a[@]}"
+}
+
 scriptDir=$(dirname "$0")
 topLevelDir=$(git -C "$scriptDir" rev-parse --show-toplevel)
 gitDir="$(git -C "$scriptDir" rev-parse --git-dir)"
@@ -23,6 +47,5 @@ if [[ -f "$gitDir/projectile" ]] ; then
     echo "Projectile root: $root" >> "$outputFile"
 fi
 cd "$topLevelDir"
-git ls-files | grep "^$root" | ctags -e --tag-relative -L - -f"$tmpTagsFile" --languages=-Vera,REXX >> "$outputFile" 2>&1
+gentags "$tmpTagsFile" "$root">> "$outputFile" 2>&1
 mv "$tmpTagsFile" "$topLevelDir/.tags"
-
