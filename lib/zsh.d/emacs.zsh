9ce5b149a2cea69f245f18ef9b9b309dcfaafb3a
Jan Sorensen <sorensenjan@gmail.com>
2016-03-28 08:57:34 +0200

    Better handling of errors
    In particular better handling if current buffer in emacs
    isn't opened on a file

--- lib/zsh.d/emacs.zsh
+++ lib/zsh.d/emacs.zsh
@@ -8,12 +8,13 @@ export VISUAL=$EDITOR
 # Print the file of the current Emacs buffer
 efile() {
     emacsclient -e '(with-current-buffer (window-buffer (selected-window)) buffer-file-name)' 2>/dev/null | read d
-    [[ "${pipestatus[1]}" -ne 0 || $d = 'nil' ]] && echo "Failed to get directory from emacs" >&2 && return 1
+    [[ "${pipestatus[1]}" -ne 0 || $d = 'nil' ]] && echo "Failed to get current file from emacs" >&2 && return 1
     echo ${d:Q}
 }
 
 edir() {
-    echo "${$(efile):h}"
+    local file
+    file=$(efile) && echo "${file:h}"
 }
 
 cde() {
