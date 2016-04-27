5da24c7dee14ce77d354d562b430a2dc5824a338
Jan Sorensen <sorensenjan@gmail.com>
2016-04-27 16:26:07 +0200

    Add hide/show for xml

--- lib/emacsinit.el
+++ lib/emacsinit.el
@@ -376,6 +376,22 @@
   (setq wgrep-auto-save-buffer t))
 
 
+;;; xml
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
 ;;; IBuffer
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
