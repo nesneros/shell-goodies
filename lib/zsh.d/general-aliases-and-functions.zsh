7dd3a67dff02baa910069c2c9ac0c1f5c3d3d262
Jan Sorensen <sorensenjan@gmail.com>
2016-04-27 16:22:36 +0200

    Fix mvl, use " around files

--- lib/zsh.d/general-aliases-and-functions.zsh
+++ lib/zsh.d/general-aliases-and-functions.zsh
@@ -150,10 +150,10 @@ function myip {
 imv() {
     local src dst
     for src; do
-        [[ -e $src ]] || { print -u2 "$src does not exist"; continue }
-        dst=$src
+        [[ -e "$src" ]] || { print -u2 "$src does not exist"; continue }
+        dst="$src"
         vared dst
-        [[ $src != $dst ]] && mkdir -p $dst:h && mv -n $src $dst
+        [[ "$src" != "$dst" ]] && mkdir -p "${dst:h}" && mv -n "$src" "$dst"
     done
 }
 
