f145cf9da0a1939005546434b43501189b313530
Jan Sorensen <sorensenjan@gmail.com>
2016-04-27 16:24:51 +0200

    Check if opened from Nautilus

--- lib/zsh.d/current-dir.zsh
+++ lib/zsh.d/current-dir.zsh
@@ -4,10 +4,11 @@ _store_pwd() {
     pwd >| $currentDirFile
 }
 
-if [[ -f "$currentDirFile" ]]; then
+# INSIDE_NAUTILUS_PYTHON is defined when opening terminal from Nautilus. In that
+# case don't change dir
+if [[ -z "${INSIDE_NAUTILUS_PYTHON+defined}" && -f "$currentDirFile" ]]; then
     local dir="$(<$currentDirFile)"
     [[ -d "$dir" ]] && cd "$dir"
 fi
 
 chpwd_functions=( ${chpwd_functions[@]} _store_pwd )
-
