ede7341ce5f55d7da7516b0238502298d32c2d19
Jan Sorensen <sorensenjan@gmail.com>
2016-11-05 12:46:52 +0100

    Fix bug in checking git template dir

--- lib/zsh.d/git.zsh
+++ lib/zsh.d/git.zsh
@@ -1,4 +1,4 @@
-local expectedTemplateDir=${SHELL_GOODIES_ROOT/$HOME/\~}/lib/git_template
+local expectedTemplateDir=${SHELL_GOODIES_ROOT}/lib/git_template
 local configuredTemplateDir=$(git config init.templateDir)
 
 if [[ "$expectedTemplateDir" != "$configuredTemplateDir" ]]; then
@@ -6,11 +6,11 @@ if [[ "$expectedTemplateDir" != "$configuredTemplateDir" ]]; then
         echo "You have non Git template dir configured."
     else
         echo "The Goodie git template dir is: $expectedTemplateDir"
-        echo "You have configured: $configuredTemplateDir"
+        echo "You have configured:            $configuredTemplateDir"
     fi
     echo
     echo "Consider executing:"
-    echo "   git config --global init.templatedir $SHELL_GOODIES_ROOT/git_template"
+    echo "   git config --global init.templatedir $expectedTemplateDir"
 fi
 if zstyle -m ':shell-goodies:dvcs:git' clonedir '*'; then
     function _git_clone {
@@ -25,10 +25,10 @@ if zstyle -m ':shell-goodies:dvcs:git' clonedir '*'; then
     alias -s git=_git_clone
 fi
 
-if [[ -n "$BREW_PREFIX" ]] && [[ "$BREW_PREFIX/bin/zsh" = "$SHELL" ]]; then
-    # Use git completion from system zsh.
-    local git_completion=$(echo /usr/share/zsh/[[:digit:]].*/functions/_git(N[-1]))
-    if [[ -f "$git_completion" ]] ; then
-        ln -sf "$git_completion" "$HOME/.goodies/fpath/_dir"
-    fi
-fi
+                            if [[ -n "$BREW_PREFIX" ]] && [[ "$BREW_PREFIX/bin/zsh" = "$SHELL" ]]; then
+                                # Use git completion from system zsh.
+                                local git_completion=$(echo /usr/share/zsh/[[:digit:]].*/functions/_git(N[-1]))
+                                if [[ -f "$git_completion" ]] ; then
+                                    ln -sf "$git_completion" "$HOME/.goodies/fpath/_dir"
+                                fi
+                            fi
