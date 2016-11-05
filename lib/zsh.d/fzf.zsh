0db3c894e859e2f37d3a800f4f8d484f9c64b146
Jan Sorensen <sorensenjan@gmail.com>
2016-11-05 15:48:23 +0100

    Add sourcing of fzf directly
    Don't use the generated scripts created by fzf install

--- lib/zsh.d/fzf.zsh
+++ lib/zsh.d/fzf.zsh
@@ -1,1 +1,13 @@
-[ -f "$HOME/.fzf.zsh" ] && source "$HOME/.fzf.zsh"
+fzfDir=$SHELL_GOODIES_ROOT/thirdparty/fzf
+
+if [[ -e $fzfDir ]] ; then
+    if [[ ! "$MANPATH" == *${fzfDir}/man* && -d "$fzfDir/man" ]]; then
+        export MANPATH="$MANPATH:$fzfDir/man"
+    fi
+
+    [[ $- == *i* ]] && source "$fzfDir/shell/completion.zsh" 2> /dev/null
+
+    source "$fzfDir/shell/key-bindings.zsh"
+else
+    echo "FZF installation dir ($fzfDir) does not exists"
+fi
